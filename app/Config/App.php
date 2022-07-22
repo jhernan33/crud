31800646b48ab464c07a139c9e6a16846d22070f
Hernan <jhernan33@gmail.com>
2022-07-22 17:21:25 -0400

    arreglo del App.php configuracion del dominio servidor

--- app/Config/App.php
+++ app/Config/App.php
@@ -24,7 +24,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'https://license.cards/:80/';
+    public $baseURL = 'http://35.87.91.131:80/';
 
     /**
      * --------------------------------------------------------------------------
